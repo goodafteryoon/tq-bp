9e55f3ab625725639c1b8eeacf127ee2946570c1
yunna <homer_j@hanmail.net>
2024-05-20 23:21:55 +0900

    feat : reuse example

--- src/components/CountriesSelect.tsx
+++ src/components/CountriesSelect.tsx
@@ -1,7 +1,11 @@
+import { useCountries } from '../hooks/useCountries';
+
 const CountriesSelect = () => {
+  const { data: country } = useCountries(); // 이제 한 줄로 api 로직을 추가할 수 있다
+
   return (
     <div>
-      <p>Select the country</p>
+      <p>Select the country </p>
     </div>
   );
 };
